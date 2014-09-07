66ac6c8d3cd963d6540f78c55da092dde5bd9623
Patrick Woods <patrickjwoods@gmail.com>
2014-09-07 14:23:59 -0500

    add amazon S3 for paperclip

--- Gemfile
+++ Gemfile
@@ -11,6 +11,7 @@ gem 'jbuilder', '~> 2.0'
 gem 'bootstrap-sass'
 gem 'devise', '~> 3.1.0.rc2'
 gem 'paperclip', github: 'thoughtbot/paperclip'
+gem 'aws-sdk', '~> 1.20.0'
 
 # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
 gem 'spring', group: :development
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,5 +84,14 @@ Rails.application.configure do
   # set this to actual hostname
   config.action_mailer.default_url_options = { :host => 'http://pjwpin.herokuapp.com/' }
 
+  # paperclip to S3 
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['AWS_BUCKET'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
 
 end
